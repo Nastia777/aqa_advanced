61ffe9d72d835ebf9397d0b696939e3eeb5c837e
Anastasiia Spivakina <nastonysh34@gmail.com>
2024-09-11 15:01:17 +0200

    task 2

---
+++ jsBasics/task2.js
@@ -0,0 +1,9 @@
+let name1 = "Nastia";
+let name2 = "Olena";
+let congratulation = "Congratulations" + " " + name1 + " and " + name2 + "!";
+
+console.log(congratulation);
+
+    let templateString = `Congratulations ${name1} and ${name2}!`;
+
+console.log(templateString);
