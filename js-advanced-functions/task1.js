22706c9b226e46fef2687365e341784d58457354
Anastasiia Spivakina <nastonysh34@gmail.com>
2024-09-14 20:21:50 +0200

    Task 1

---
+++ js-advanced-functions/task1.js
@@ -0,0 +1,19 @@
+const handleEven = () => {
+    console.log("Number is even");
+}
+
+const handleOdd = () => {
+    console.log("Number is odd");
+}
+
+const handleNum = (number, evenCallback, oddCallback) => {
+    if (number % 2 === 0) {
+        evenCallback();
+    }
+    else { 
+        oddCallback();
+    }
+}
+
+handleNum(4, handleEven, handleOdd);
+handleNum(1, handleEven, handleOdd);
