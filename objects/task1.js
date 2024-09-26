40c2661fe4056a8ae9655e6199418e2b6d8f5966
Anastasiia Spivakina <nastonysh34@gmail.com>
2024-09-26 17:17:49 +0200

    Task 1

---
+++ objects/task1.js
@@ -0,0 +1,11 @@
+const student = {
+    firstName: "Anastasiia",
+    lastName: "Spivakina",
+    age: 24,
+    courses: ["Math", "History", "Programming"]
+}
+
+console.log(student);
+
+//just for me
+console.log(student.courses[1]); 
