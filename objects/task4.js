7ac1cc3d04438f9638cb6f6a51b7c3bfc7751538
Anastasiia Spivakina <nastonysh34@gmail.com>
2024-09-26 17:29:45 +0200

    Task 4

---
+++ objects/task4.js
@@ -0,0 +1,12 @@
+const person = {
+    firstName: "Anastasiia",
+    lastName: "Spivakina",
+    age: 24
+}
+
+console.log(person);
+person.email = "nastonysh34@gmail.com"
+console.log(person);
+
+delete person.age;
+console.log(person);
